4cea1896d01a652da7040b9d1a071b6e6dc131d5
Vineeth Chelur <crvineeth97@gmail.com>
2019-11-16 11:45:10 +0530

    Update contract to include parallel running games

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,23 +1,40 @@
 pragma solidity >= 0.4.21 < 0.6.0;
 
-contract TicTacToe {
+contract TicTacToe
+{
     // Address of the two players and the owner
     address payable public owner;
-
-    address payable public host;
-    address payable public joiner;
-    uint public state = 0;
-    uint public current_move = 0;
-    uint public balance = 0;
-    uint public match_count = 0;
-    uint public host_win = 0;
-    uint public joiner_win = 0;
     enum states {E, X, O}
-    states[3][3] public board;
 
+    struct Game
+    {
+        address payable host;
+        address payable joiner;
+        uint8 state;
+        uint8 current_move;
+        uint balance;
+        uint8 match_count;
+        uint8 host_win;
+        uint8 joiner_win;
+        states[3][3] board;
+    }
 
-    constructor() public {
+    mapping(uint8 => Game) public games;
+
+    constructor()
+        public
+    {
         owner = msg.sender;
+    }
+
+    // Random number generator. Due to the constraints of blockchain TM, not perfectly random: but it will do
+    // Generates a random number between 0 and 51.
+    function generateGameID()
+        internal
+        view
+        returns (uint8)
+    {
+        return uint8(uint256(keccak256(abi.encodePacked(now, block.difficulty))) % 52);
     }
 
     // Simple State Machine
@@ -26,33 +43,38 @@ contract TicTacToe {
     // State 2: Challenger has joined
     // State 3: Game Over
 
-    function start() public payable {
-        require(state == 0);
-        require(msg.value >= 1);
-        require(balance == 0);
+    function create()
+        public
+        payable
+        returns (uint8)
+    {
+        require(msg.value >= 1, "Send a minimum of 1 Wei");
+        require(msg.sender != owner, "Owner of contract can't take part in the game");
 
-        host = msg.sender;
-        balance += msg.value;
-        state = 1;
+        uint8 id = generateGameID();
+        // If there are 52 games, make a check to not create more games
+        while (games[id].host != address(0))
+            id = generateGameID();
+
+        games[id].host = msg.sender;
+        games[id].balance = msg.value;
+        games[id].state = 1;
+        return id;
     }
 
-    function join(address _host) public payable {
-        require(_host == host);
-        require(msg.value >= 1);
-        require(state == 1);
-        require(_host != msg.sender);  // Making sure that host is not trying to play with himself
+    function join(uint8 id)
+        public
+        payable
+    {
+        require(games[id].state == 1, "Player can only join an existing game. Wrong ID");
+        require(msg.value >= 1, "Send a minimum of 1 Wei");
+        require(owner != msg.sender, "Owner of contract can't take part in the game");
+        require(games[id].host != msg.sender, "Making sure that host is not trying to play with himself");
 
-        joiner = msg.sender;
-        balance += msg.value;
-        state = 2;
+        games[id].joiner = msg.sender;
+        games[id].balance += msg.value;
+        games[id].state = 2;
     }
-
-    // constructor(address _joiner) public {
-    //     require(_joiner != address(0));
-
-    //     host = msg.sender;
-    //     joiner = _joiner;
-    // }
 
     // Checks to be performed before the transaction is allowed to make a change to the board
     // - Make sure the host and Challenger have joined the game
@@ -60,188 +82,197 @@ contract TicTacToe {
     // - The game is not over and it is their turn
     // - The arguments are within bounds of the board
     // - If the specified block on the board is empty
-    function MakeMove(uint y, uint x) public {
-        require (state == 2);
-        require (msg.sender == host || msg.sender == joiner);
-        require (!isGameOver() && msg.sender == currentPlayerAddress());
-        require (positionCheck(x, y));
-        require (board[x][y] == states.E);
+    function MakeMove(uint8 id, uint8 y, uint8 x)
+        public
+    {
+        require (games[id].state == 2, "Player has not joined the game");
+        require (msg.sender == games[id].host || msg.sender == games[id].joiner, "Only the host and joiner can play this game");
+        require (!isMatchOver(id), "Won't come here");
+        require (msg.sender == currentPlayerAddress(id), "Please wait for your turn");
+        require (positionCheck(x, y), "Ensure that position is inside the board");
+        require (games[id].board[x][y] == states.E, "This board position is already filled");
 
-        // TODO
-        if(currentPlayerAddress() == joiner) {
-            board[x][y] = states.X;
-        }
-        else {
-            board[x][y] = states.O;
-        }
+        if(currentPlayerAddress(id) == games[id].joiner)
+            games[id].board[x][y] = states.X;
+        else
+            games[id].board[x][y] = states.O;
 
-        current_move = current_move + 1;
+        games[id].current_move = games[id].current_move + 1;
 
-        if(isGameOver()) {
-            state = 3;
-            EndGame();
+        if(isMatchOver(id))
+        {
+            games[id].state = 3;
+            endGame(id);
         }
     }
 
     // Utility function to make sure we never check positions outside the board
-    function positionCheck(uint xpos, uint ypos) private pure returns (bool) {
+    function positionCheck(uint8 xpos, uint8 ypos)
+        private
+        pure
+        returns (bool)
+    {
         return (xpos >= 0 && xpos < 3 && ypos >= 0 && ypos < 3);
     }
 
     // Match 1 & 3: Joiner plays first
     // Match 2 & 4: Host plays first
-    function currentPlayerAddress() public view returns (address) {
-        if ((current_move+match_count) % 2 == 0) {
-            return joiner;
-        }
-        else {
-            return host;
-        }
+    function currentPlayerAddress(uint8 id)
+        public
+        view
+        returns (address)
+    {
+        if ((games[id].current_move + games[id].match_count) % 2 == 0)
+            return games[id].joiner;
+        else
+            return games[id].host;
     }
 
     // Game is over when 9 moves have been made or when one of the player wins
-    function isGameOver() public view returns (bool) {
-        require(state == 2);
-        return (current_move > 8 || winningPlayerShape() != states.E);
+    function isMatchOver(uint8 id)
+        public
+        view
+        returns (bool)
+    {
+        require(games[id].state == 2, "Ensure that match has completed");
+        return (games[id].current_move > 8 || winningPlayerShape(id) != states.E);
     }
 
-    // After the game is over sendin the money to the winner
-    function EndGame() private {
-        require(state == 3);
-        require(balance > 0);
+    // After the game is over, send the money to the winner
+    function endGame(uint8 id)
+        private
+    {
+        require(games[id].state == 3, "Ensure that the game ended");
+        require(games[id].balance > 0, "Balance should be greater than 0. Won't come here");
 
-        if(winner() == host) {
-            host_win = host_win+1;
-        }
-        else if(winner() == joiner) {
-            joiner_win = joiner_win+1;
-        }
-        match_count = match_count + 1;
+        if (winner(id) == games[id].host)
+            games[id].host_win = games[id].host_win + 1;
+        else if(winner(id) == games[id].joiner)
+            games[id].joiner_win = games[id].joiner_win + 1;
 
-        if(match_count == 4) {
-            if( joiner_win == host_win) {
-                owner.transfer(balance);
-            }
-            else if(joiner_win > host_win) {
-                joiner.transfer(balance);
-            }
-            else if(host_win > joiner_win) {
-                host.transfer(balance);
-            }
-            reset();
+        games[id].match_count = games[id].match_count + 1;
+
+        if(games[id].match_count == 4)
+        {
+            if( games[id].joiner_win == games[id].host_win)
+                owner.transfer(games[id].balance);
+            else if(games[id].joiner_win > games[id].host_win)
+                games[id].joiner.transfer(games[id].balance);
+            else if(games[id].host_win > games[id].joiner_win)
+                games[id].host.transfer(games[id].balance);
+            reset(id);
         }
-        else {
-            state = 2;
-            // balance = 0;
-            current_move = 0;
-            // host_win = 0;
-            // joiner_win = 0;
-            // match_count = 0;
-            reset_board();
+        else
+        {
+            games[id].state = 2;
+            games[id].current_move = 0;
+            for(uint8 r = 0; r < 3; r++)
+            {
+                for(uint8 s = 0; s < 3; s++)
+                {
+                    games[id].board[r][s] = states.E;
+                }
+            }
         }
     }
 
     // We have to make sure that the money has been transferred to the winner or the owner
-    function reset() private {
-        require(state == 3);
-
-        state = 0;
-        balance = 0;
-        current_move = 0;
-        host_win = 0;
-        joiner_win = 0;
-        match_count = 0;
-        reset_board();
-    }
-
-    //resets board to default values
-    function reset_board() private {
-        for(uint r = 0; r < 3; r++) {
-            for(uint s = 0; s < 3; s++) {
-                board[r][s] = states.E;
-            }
-        }
+    function reset(uint8 id)
+        private
+    {
+        require(games[id].state == 3, "Ensure that game has ended");
+        delete games[id];
     }
 
     // Utility function
-    function check(uint r1, uint c1, uint r2, uint c2, uint r3, uint c3)
-    private view returns (states) {
-        if(board[r1][c1] != states.E && board[r1][c1] == board[r2][c2] && board[r2][c2] == board[r3][c3])
-            return board[r1][c1];
+    function check(uint8 id, uint8 r1, uint8 c1, uint8 r2, uint8 c2, uint8 r3, uint8 c3)
+        private
+        view
+        returns (states)
+    {
+        if(games[id].board[r1][c1] != states.E &&
+            games[id].board[r1][c1] == games[id].board[r2][c2] &&
+            games[id].board[r2][c2] == games[id].board[r3][c3])
+            return games[id].board[r1][c1];
         else
             return states.E;
     }
 
     // Function to check if the board has a winner and returns the winning shape
-    function winningPlayerShape() private view returns (states) {
+    function winningPlayerShape(uint8 id)
+        private
+        view
+        returns (states)
+    {
         // Check rows
-        for(uint r = 0; r < 3; r++) {
-            if(check(r, 0, r, 1, r, 2) != states.E)
-                return check(r, 0, r, 1, r, 2);
+        for(uint8 r = 0; r < 3; r++) {
+            if(check(id, r, 0, r, 1, r, 2) != states.E)
+                return check(id, r, 0, r, 1, r, 2);
         }
 
         // Check coloumns
-        for(uint r = 0; r < 3; r++) {
-            if(check(0, r, 1, r, 2, r) != states.E)
-                return check(0, r, 1, r, 2, r);
+        for(uint8 r = 0; r < 3; r++) {
+            if(check(id, 0, r, 1, r, 2, r) != states.E)
+                return check(id, 0, r, 1, r, 2, r);
         }
 
         // Check diagonals
 
-        if(check(0, 0, 1, 1, 2, 2) != states.E)
-            return check(0, 0, 1, 1, 2, 2);
-        if(check(0, 2, 1, 1, 2, 0) != states.E)
-            return check(0, 2, 1, 1, 2, 0);
+        if(check(id, 0, 0, 1, 1, 2, 2) != states.E)
+            return check(id, 0, 0, 1, 1, 2, 2);
+        if(check(id, 0, 2, 1, 1, 2, 0) != states.E)
+            return check(id, 0, 2, 1, 1, 2, 0);
 
         return states.E;
     }
 
     // Function for the outside world to see who the winner is
-    function winner() public view returns (address payable) {
-        states winning_shape = winningPlayerShape();
+    function winner(uint8 id)
+        public
+        view
+        returns (address payable)
+    {
+        states winning_shape = winningPlayerShape(id);
         if(winning_shape == states.X) {
-            return joiner;
+            return games[id].joiner;
         }
         else if(winning_shape == states.O) {
-            return host;
+            return games[id].host;
         }
-
         return address(0);
     }
 
     // Drawing the board
-
-    function stateToString() public view returns (string memory) {
-        return string(abi.encodePacked("\n",
-            colToString(0), "\n",
-            colToString(1), "\n",
-            colToString(2), "\n"
+    function stateToString(uint8 id)
+        public
+        view
+        returns (string memory)
+    {
+        return string(
+            abi.encodePacked(
+            colToString(id, 0), "\n",
+            colToString(id, 1), "\n",
+            colToString(id, 2), "\n"
         ));
     }
 
-    function colToString(uint pos) private view returns (string memory) {
-        return string(abi.encodePacked(squareToString(0, pos), "|", squareToString(1, pos), "|", squareToString(2, pos)));
+    function colToString(uint8 id, uint8 pos)
+        private
+        view
+        returns (string memory)
+    {
+        return string(abi.encodePacked(squareToString(id, 0, pos), "|", squareToString(id, 1, pos), "|", squareToString(id, 2, pos)));
     }
 
-    function squareToString(uint xpos, uint ypos) private view returns (string memory) {
-        require (positionCheck(xpos, ypos));
-
-        if(board[xpos][ypos] == states.E) {
-            return " ";
-        }
-        else if(board[xpos][ypos] == states.X) {
+    function squareToString(uint8 id, uint8 xpos, uint8 ypos)
+        private
+        view
+        returns (string memory)
+    {
+        if (games[id].board[xpos][ypos] == states.X)
             return "X";
-        }
-        else if(board[xpos][ypos] == states.O) {
+        else if (games[id].board[xpos][ypos] == states.O)
             return "O";
-        }
         return " ";
-    }
-
-    function sendToOwner() public {
-        require(msg.sender == owner);
-        if(balance > 0) {
-            owner.transfer(balance);
-        }
     }
 }
